69f930e237ad4828a3bca888e27b7db6c0edd5ab
Thunderbolt2812 <Gustavopineda95@gmail.com>
2021-01-29 15:04:58 -0600

    Mark 3

--- P3Lab2_GustavoPineda.cpp
+++ P3Lab2_GustavoPineda.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-#include <iostream>
 #include <array>
 #include <cstdlib>
 
@@ -9,17 +8,28 @@
 using namespace std;
 
 void ordenAscen(int array[], int num){
-    if (num == 1)
+    
+	if (num == 1)
         return;
-
     for (int i=0; i<num-1; i++)
-        if (array[i] > array[i+1])
-        {
+        if (array[i] > array[i+1]){
             int aux = array[i];
             array[i] = array[i+1];
             array[i+1] = aux;
         }
+        for(int p = 0;p<20;p++){
+			cout<<array[p]<<",";
+		}
+		cout<<endl;
     ordenAscen(array, num-1);
+}
+float sumatoria(int num, float acum){
+	if(num==1){
+		acum=acum+(((2*num)-1)/(num*(num+1)));
+		return acum;
+	}
+	acum= acum+(((2*num)-1)/(num*(num+1)));
+	sumatoria(num-1,acum);
 }
 
 int main(int argc, char** argv) {
@@ -39,21 +49,20 @@ int main(int argc, char** argv) {
 			    for(int i = 0;i<20;i++){
 			    	arreglo[i] = 1 + (rand()%99);
 				}
-				cout<<"Array desordenado"<<endl;
-				for(int j = 0;j<20;j++){
-						cout<<arreglo[j]<<",";
-				}
-				cout<<endl;
 			    ordenAscen(arreglo, 20);
 			    cout<<endl;
-				cout<<"Array Ordenado de forma ascendente"<<endl;;
-			    for(int p = 0;p<20;p++){
-			    	cout<<arreglo[p]<<",";
-				}
-				cout<<endl;
-				cout<<endl;
 				break;
 			case 3:
+				int num;
+				double acum=0;
+				cout<<"Ingrese un numero"<<endl;
+				cin>>num;
+				while(num<=0){
+					cout<<"Porfavor ingrese un numero positivo diferente de 0"<<endl;
+					cin>>num;
+				}
+				float resp = sumatoria(num,acum);
+				cout<<"S("<<num<<") = "<<resp<<endl;
 				break;
 		}
 	}
